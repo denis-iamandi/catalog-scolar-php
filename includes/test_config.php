ccad825bbbce6bdec2b24ab604f6fe0064cdc9db
denis-iamandi <iamandidenis32@gmail.com>
2026-01-05 22:06:50 +0200

    Add files via upload

---
+++ includes/test_config.php
@@ -0,0 +1,17 @@
+<?php
+// Include fișierul de configurare
+include "config.php";
+
+// Dacă s-a ajuns aici, conexiunea a reușit
+echo "<h1 style='color: green; text-align: center; margin-top: 40px;'>
+✔ Conexiunea la baza de date funcționează corect!
+</h1>";
+
+echo "<p style='text-align: center; font-size: 18px;'>
+Fișierul <strong>config.php</strong> a fost încărcat cu succes și conexiunea MySQL este activă.
+</p>";
+
+echo "<p style='text-align: center; color: #555;'>
+Baza de date utilizată: <strong>educatie</strong>
+</p>";
+?>
